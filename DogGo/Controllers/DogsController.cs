b31c4c51277e37a6f48ea2bf16af628cf4c96c75
Brendan Abernethy <brendan.j.abernethy@gmail.com>
2020-09-17 16:00:24 -0500

    added create and detail view to dog view

--- DogGo/Controllers/DogsController.cs
+++ DogGo/Controllers/DogsController.cs
@@ -21,7 +21,13 @@ namespace DogGo.Controllers
         // GET: DogsController/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            Dog dog = _dogRepo.GetDogById(id);
+
+            if (dog == null)
+            {
+                return NotFound();
+            }
+            return View(dog);
         }
 
         // GET: DogsController/Create
@@ -33,15 +39,16 @@ namespace DogGo.Controllers
         // POST: DogsController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public ActionResult Create(Dog dog)
         {
             try
             {
-                return RedirectToAction(nameof(Index));
+                _dogRepo.AddDog(dog);
+                return RedirectToAction("Index");
             }
-            catch
+            catch (Exception ex)
             {
-                return View();
+                return View(dog);
             }
         }
 
--- DogGo/Repositories/DogRepository.cs
+++ DogGo/Repositories/DogRepository.cs
@@ -93,9 +93,19 @@ namespace DogGo.Repositories
                             Name = reader.GetString(reader.GetOrdinal("Name")),
                             OwnerId = reader.GetInt32(reader.GetOrdinal("OwnerId")),
                             Breed = reader.GetString(reader.GetOrdinal("Breed")),
-                            Notes = reader.GetString(reader.GetOrdinal("Notes")),
-                            ImageUrl = reader.GetString(reader.GetOrdinal("ImageUrl"))
+                           // Notes = reader.GetString(reader.GetOrdinal("Notes")),
+                           // ImageUrl = reader.GetString(reader.GetOrdinal("ImageUrl"))
                         };
+
+                        // Check if optional columns are null
+                        if (reader.IsDBNull(reader.GetOrdinal("Notes")) == false)
+                        {
+                            dog.Notes = reader.GetString(reader.GetOrdinal("Notes"));
+                        }
+                        if (reader.IsDBNull(reader.GetOrdinal("ImageUrl")) == false)
+                        {
+                            dog.ImageUrl = reader.GetString(reader.GetOrdinal("ImageUrl"));
+                        }
 
                         reader.Close();
                         return dog;
