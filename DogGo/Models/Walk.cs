3f019140c1a2a51ec32339f428ee86ede4140fc2
Brendan Abernethy <brendan.j.abernethy@gmail.com>
2020-09-22 12:11:42 -0500

    fixed date format

--- DogGo/Models/Walk.cs
+++ DogGo/Models/Walk.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,6 +11,7 @@ namespace DogGo.Models
     {
         public int Id { get; set; }
 
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
         public DateTime Date { get; set; }
 
         public int Duration { get; set; }
